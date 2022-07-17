3b0ea7d0d400ae6ec7b8c6342d718df46b79dae4
elenaJO <ele.lucero1993@gmail.com>
2022-07-17 13:14:21 -0500

    feat(MAQ): maquetación banner

---
+++ src/components/home/HomeBanner.tsx
@@ -0,0 +1,17 @@
+export const HomeBanner = () => {
+	return (
+		<div className="home-banner">
+			<div className="home-banner__content-text">
+				<p className="home-banner__text--small">¡NUEVO!</p>
+				<p className="home-banner__text--title">
+					Seguro Vehicular <br />
+					<span className="home-banner__text--title home-banner__text--red">Tracking</span>
+				</p>
+				<p className="home-banner__text--subtitle">Cuentanos donde le haras seguimiento a tu seguro</p>
+			</div>
+			<div className="home-banner__content-image">
+				<div className="home-banner__image"></div>
+			</div>
+		</div>
+	)
+}

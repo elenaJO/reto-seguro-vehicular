dc914c955a8ed967b652e543d2eb9b014ad06460
elenaJO <ele.lucero1993@gmail.com>
2022-07-17 11:26:26 -0500

    feat(MAQ): maquetación checkbox y radio button

---
+++ src/components/shared/CheckBoxApp.tsx
@@ -0,0 +1,25 @@
+interface SelectArgs {
+	checked?: boolean,
+	handleCheckChange?: (e: any) => void,
+	name?: string,
+}
+
+export const CheckBoxApp = ({ checked, handleCheckChange, name }: SelectArgs) => {
+	return (
+		<div className="check-box-app">
+			<input
+				className="check-box-app__box"
+				type="checkbox" 
+				id="checkApp"
+				checked={ checked }
+				name={ name }
+				onChange={ handleCheckChange }
+			/>
+    	<label
+				className="check-box-app__label"
+				htmlFor="checkApp"
+			>
+			</label>
+		</div>
+	)
+}
---
+++ src/components/shared/RadioBtnApp.tsx
@@ -0,0 +1,28 @@
+interface RadioBtnArgs {
+	optionalClass?: string,
+	name?: string,
+	value?: string | number,
+	handleCheckChange?: (e: any) => void,
+	checked?: boolean,
+}
+
+export const RadioBtnApp = ({ optionalClass, value, name, handleCheckChange, checked }: RadioBtnArgs) => {
+	return (
+		<div className={`${optionalClass} radio-btn-app`}>
+			<input
+				className="radio-btn-app__radio"
+				type="radio" 
+				id={`${name}-${value}`}
+				value={ value }
+				name={ name }
+				checked={ checked }
+				onChange={ handleCheckChange }
+			/>
+    	<label
+				className="radio-btn-app__label"
+				htmlFor={`${name}-${value}`}
+			>{ value }
+			</label>
+		</div>
+	)
+}

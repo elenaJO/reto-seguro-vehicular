6449361922765a19cd4eebf5e0fa9b8549a4f930
elenaJO <ele.lucero1993@gmail.com>
2022-07-18 11:38:52 -0500

    FEAT(INT): Implentar redux

--- src/VehicleApp.tsx
+++ src/VehicleApp.tsx
@@ -1,9 +1,13 @@
 import { AppRouter } from "./routers/AppRouter"
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export const VehicleApp = () => {
 	return (
 		<div>
+			<Provider store={ store }>
 				<AppRouter/>
+			</Provider>
 		</div>
 	)
 }

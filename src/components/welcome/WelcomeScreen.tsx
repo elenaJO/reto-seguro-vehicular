2324114aea971096cb99b861750595715d6bd6ec
elenaJO <ele.lucero1993@gmail.com>
2022-07-18 13:22:14 -0500

    feat(INT): Maquetación pantalla bienvenida y agregar datos de usuario

---
+++ src/components/welcome/WelcomeScreen.tsx
@@ -0,0 +1,11 @@
+import { FooterApp } from "../shared/FooterApp"
+import { WelcomeBanner } from "./WelcomeBanner"
+
+export const WelcomeScreen = () => {
+	return (
+		<>
+			<WelcomeBanner/>
+			<FooterApp/>
+		</>
+	)
+}
